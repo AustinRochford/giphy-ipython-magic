37bc3c9eeeacae3ee3799d06404cdda6860cde4e
AustinRochford <arochford@monetate.com>
2015-02-24 08:52:14 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='giphy-ipython-magic',
+    version='0.0.1',
+    author="Austin Rochford",
+    author_email="austin.rochford@gmail.com",
+    description="giphy line magic for ipython 2.0+",
+    install_requires=['ipython>=2.0', 'requests'],
+    keywords="ipython",
+    license="MIT",
+    long_description="""This package provides a line magic for embedding random GIFs from giphy.com in IPython notebooks""",
+    platforms='any',
+    py_modules=['giphy_magic'],
+    url="https://github.com/AustinRochford/giphy-ipython-magic",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
